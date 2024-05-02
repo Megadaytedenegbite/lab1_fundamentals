ba594b3a4a4ed82fa2a8d786ad0509e3b98d2291
Mega <jargamer9@gmail.com>
2024-05-02 22:00:22 +0300

    обновил TextScreen.kt. обновил fun MyText(). обновил дизайн в MyText().

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
@@ -1,4 +1,5 @@
 package com.raywenderlich.android.lab1.screens
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -6,12 +7,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+
 @Composable
 fun TextScreen() {
     Column(
@@ -21,16 +26,18 @@ fun TextScreen() {
     ) {
         MyText()
     }
+
     BackButtonHandler {
         FundamentalsRouter.navigateTo(Screen.Navigation)
     }
 }
 
 @Composable
-@Preview
 fun MyText() {
-    //TODO add your code here
-    Text(
-        text = stringResource(id = R.string.set_text)
+    Text(text = stringResource(id = R.string.set_text),
+        fontStyle = FontStyle.Italic,
+        color = colorResource(id = R.color.black),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
     )
 }
