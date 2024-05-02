2af55da766f2da33769660a74b1f7228971396a0
Mega <jargamer9@gmail.com>
2024-05-02 21:47:43 +0300

    Обновил TextScreen.kt. Обновил функцию MyText(). Добавил Text() в MyText(). Добавил @Preview в MyText().

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
@@ -1,15 +1,17 @@
 package com.raywenderlich.android.lab1.screens
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-
 @Composable
 fun TextScreen() {
     Column(
@@ -19,13 +21,16 @@ fun TextScreen() {
     ) {
         MyText()
     }
-
     BackButtonHandler {
         FundamentalsRouter.navigateTo(Screen.Navigation)
     }
 }
 
 @Composable
+@Preview
 fun MyText() {
     //TODO add your code here
+    Text(
+        text = stringResource(id = R.string.set_text)
+    )
 }
