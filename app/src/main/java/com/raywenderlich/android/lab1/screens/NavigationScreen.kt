72619f27576f6fb7037abac560da6f7b6b51d889
Mega <jargamer9@gmail.com>
2024-05-03 11:43:31 +0300

    Добавлена сортировка книг(изображений) по категориям, возможность прокрутки категорий по вертикали и просмотр книг одной категории прокруткой по горизонтали Дополнены файлы: NavigationScreen.kt, strings.xml.

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -39,6 +39,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_container_surface), Screen.Surface)
             NavigationButton(stringResource(id = R.string.button_container_scaffold), Screen.Scaffold)
             NavigationButton(stringResource(id = R.string.button_scrolling), Screen.Scrolling)
+            NavigationButton(stringResource(id = R.string.button_list), Screen.List)
         }
     }
 }
